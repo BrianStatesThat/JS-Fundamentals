d8a5fb6ddccfb54b07528bb254560cbee0c8618c
brianstatesthat <mbspeelman@gmail.com>
2025-07-08 19:54:29 +0200

    added 2 arguments js file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,3 +1,9 @@
-function print(first, second) {
-  console.log(first);
+const numberOfArguments = process.argv.length - 2; // Subtract 2 to exclude 'node' and the script name
+
+if (numberOfArguments === 0) {
+  console.log("No argument");
+} else if (numberOfArguments === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
