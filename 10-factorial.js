d806c522a9b10b4af5c09254d80c515bab44d437
brianstatesthat <mbspeelman@gmail.com>
2025-07-09 10:55:01 +0200

    Bonus Advanced question

---
+++ 10-factorial.js
@@ -0,0 +1,17 @@
+const factorial = (n) => {
+    if (isNaN(n) || n < 0) {
+    return 1;
+  }
+  
+  if (n === 0 || n === 1) { 
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+const argStr = process.argv[2];
+
+const num = parseInt(argStr);
+
+const result = factorial(num);
+
+console.log(result);
