7f2929732012bc280cc9fb46222b84e4a6a49ca9
brianstatesthat <mbspeelman@gmail.com>
2025-07-09 10:11:17 +0200

    Added the 6th multi language

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+const languages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+const output = languages.join('\n');
+
+// Print the single multi-line string using one console.log() call.
+console.log(output);
