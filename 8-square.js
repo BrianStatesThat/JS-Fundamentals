3d66146c76a9eb9b4284f906d0a32c7fbd8d52b5
brianstatesthat <mbspeelman@gmail.com>
2025-07-09 10:38:57 +0200

    Added the 8th file

---
+++ 8-square.js
@@ -0,0 +1,16 @@
+const sizeStr = process.argv[2];
+
+const size = parseInt(sizeStr);
+
+if (isNaN(size) || size <= 0) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = "";
+
+    for (let j = 0; j < size; j++) {
+      row += "X";
+    }
+    console.log(row);
+  }
+}
