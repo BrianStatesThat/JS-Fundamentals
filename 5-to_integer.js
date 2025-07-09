a66f53abc5814ceaeda792b8bdb6285f6f8a69cb
brianstatesthat <mbspeelman@gmail.com>
2025-07-09 10:24:06 +0200

    Added the 7 multi

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,16 @@
-const firstArgument = process.argv[2];
-const parsedInteger = parseInt(firstArgument);
+const numOccurrencesStr = process.argv[2];
+const numOccurrences = parseInt(numOccurrencesStr);
 
-if (!isNaN(parsedInteger)) {
-  console.log(`My number: ${parsedInteger}`);
+if (isNaN(numOccurrences) || numOccurrences <= 0) {
+  console.log("Missing number of occurrences"); // First console.log
 } else {
-  console.log("Not a number");
+  let output = "";
+  for (let i = 0; i < numOccurrences; i++) {
+    output += "C is fun";
+    // Add a newline ONLY if it's not the last iteration
+    if (i < numOccurrences - 1) {
+      output += "\n";
+    }
+  }
+  console.log(output); // Second console.log
 }
